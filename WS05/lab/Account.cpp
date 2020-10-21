03815758cf5f3ea23401b9ba6b9c563d03768987
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2020-10-21 12:50:10 -0400

    removed return cout (a line that was copied here by mistake)

--- WS05/lab/Account.cpp
+++ WS05/lab/Account.cpp
@@ -35,7 +35,11 @@ namespace sdds {
          cout.setf(ios::fixed);
          cout << m_balance;
          cout.unsetf(ios::right);
-         return cout;
+         // I (Fardad) removed the "return cout", this was a mistake
+         // (copied and pasted by mistake)
+         // has no effect to the execution of the program
+         // is as mentioned before we should have only
+         // one return statement in a function.
       }
       else {
          cout << "Invalid Account";
